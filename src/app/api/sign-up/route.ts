4a5b765b72249f4c984258347f10fc910b1b9546
Ajeem12 <ajeembeg98.com>
2024-12-07 15:24:44 +0530

    Add verificationEmail component, sign-up route, sendverificationEmail helper, dbconnection and Apiresponce

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,18 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+  } catch (error) {
+    console.log("Error registering user", error);
+    return Response.json(
+      { success: false, message: "Failed to register user" },
+      { status: 500 }
+    );
+  }
+}
